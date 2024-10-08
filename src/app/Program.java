86da6a2a69db5ac7bc4b7446ab289d19f3e74cfe
Ruan <ruanpablooliveira20@gmail.com>
2024-10-08 13:28:04 -0300

    Implements findById in SellerDaoJDBC class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,20 +1,14 @@
 package app;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = null;
-		PreparedStatement st = null;
-	
-		Department obj = new Department(1,"books");
-		Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.0,obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
