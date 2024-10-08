5d8492908d205fc535c40772390806479e21ddaa
Ruan <ruanpablooliveira20@gmail.com>
2024-10-08 12:56:02 -0300

    Seller class

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,8 +2,10 @@ package app;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Date;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -12,6 +14,8 @@ public class Program {
 		PreparedStatement st = null;
 	
 		Department obj = new Department(1,"books");
+		Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.0,obj);
+		System.out.println(seller);
 	}
 
 }
