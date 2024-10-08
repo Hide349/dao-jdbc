00d1ab06ecbe362061f9ddf198988eea8ea1f504
Ruan <ruanpablooliveira20@gmail.com>
2024-10-08 12:49:17 -0300

    Department class

--- src/app/Program.java
+++ src/app/Program.java
@@ -2,9 +2,8 @@ package app;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
@@ -12,22 +11,7 @@ public class Program {
 		Connection conn = null;
 		PreparedStatement st = null;
 	
-		
-		try {
-			conn = DB.getConnection();
-			st = conn.prepareStatement(
-					"UPDATE seller "+
-					"SET BaseSalary = ? "+
-					"WHERE "+
-					"(Name = ?)");
-			
-			st.setDouble(1, 3000.00);
-			st.setString(2, "Bob Brown");
-			int rowsAffected = st.executeUpdate();
-			System.out.println("rows affecteds : " + rowsAffected);
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
+		Department obj = new Department(1,"books");
 	}
 
 }
